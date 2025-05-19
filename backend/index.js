48a927ceb69c2e7f07fb7d9eed8494c8fac54a42
BigBoy-glitch <pacreal13@gmail.com>
2025-05-19 11:58:03 +0100

    oops!

--- backend/index.js
+++ backend/index.js
@@ -15,12 +15,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
-const frontendOrigin = process.env.FRONTEND_ORIGIN || 'https://www.neontrust.us';
+const frontendOrigin = process.env.FRONTEND_ORIGIN || 'https://http://localhost:5000';
 const port = process.env.PORT || 5000;
 
 // ✅ CORS setup
 app.use(cors({
-  origin: frontendOrigin, // https://www.neontrust.us
+  origin: frontendOrigin, // https://http://localhost:5000
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
